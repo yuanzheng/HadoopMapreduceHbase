cee707607d0ca8477bbbfd212a84973e756838f0
lucas <ysong@insidesales.com>
2014-09-19 14:24:00 -0600

    build the CPI and GDP import to hbase project

---
+++ DataImport/src/main/java/EconomicDataImport/DataImport/App.java
@@ -0,0 +1,13 @@
+package EconomicDataImport.DataImport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
